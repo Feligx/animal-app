51fbccbb3c88dd384ee4cdd4723108903bdaa915
dleyvacastro <dleyvacastro@gmail.com>
2022-04-28 10:52:53 -0500

    Adapted to firestore

--- Hardware/Traffic_simulator.py
+++ Hardware/Traffic_simulator.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
 import requests
 from time import sleep
 from random import uniform, randint
 # Generador de trafico para subir informacion a firebase
 # Parametros enviados a travez de la URL
 for i in range(1, 50):
-    url = f'http://45.79.41.19/cgi-bin/fb.py?id={randint(1,5)}&lat={uniform(-90,90)}&lon={uniform(-180,180)}'
+    url = f'http://45.79.41.19/cgi-bin/fs.py?idd={randint(1,5)}&latitude={uniform(-90,90)}&longitude={uniform(-180,180)}'
     print(url)
     r = requests.get(url)
     print(r.text)
